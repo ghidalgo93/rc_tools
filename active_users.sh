3ff6edaa6187bca41e3db29feabe55d0e500eb89
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-11-22 12:57:38 -0700

    initial commit

---
+++ active_users.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Program to query slurm for users that have submitted >1 job in the past N months
+
+## Natural -> Natural, list
+## Given a number of months, return number of and a list of unique users who have submitted > 1 job 
+## Stub
+
+
